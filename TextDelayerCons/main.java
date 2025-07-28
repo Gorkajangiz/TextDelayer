c5e68bcbb11f7e3ea75c8ef90d1f755f1ad516ef
Gorka Ajangiz Morales <gorkajangiz@gmail.com>
2025-07-28 15:21:07 +0200

    Actualización
    Solucionado un error que impedía seleccionar la velocidad 2.
    Código entero reescrito para introducir todo al método y no depender de nada en el main. Además, ahora el método puede recibir tanto un array de frases a imprimir como una única frase, y se ha añadido un mensaje de error funcional.

--- TextDelayerCons/main.java
+++ TextDelayerCons/main.java
@@ -8,25 +8,16 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String args[]) throws InterruptedException {
-        Scanner sc = new Scanner(System.in);
         metodo m = new metodo();
-        System.out.println("Selecciona tu velocidad, por favor:");
-        System.out.println("1. Lento");
-        System.out.println("2. Normal");
-        System.out.println("3. Rapido");
-        System.out.println("4. Rapidísimo");
-        Integer res = Integer.valueOf(sc.nextLine());
-        sc.close();
-        String[] frases = {
+
+        String[] sentences = {
             "Ejemplo 1, esta frase tiene aproximadamente 100 caracteres. Solo sirve para hacer una prueba visual.",
-            "Ejemplo 4, esta frase tiene aproximadamente 400 caracteres. A medida que el texto crece, también lo hace la necesidad de gestionar mejor la sincronización para evitar que la impresión se sienta artificial. Una animación fluida puede mejorar la experiencia del usuario y dar la sensación de que algo más complejo está ocurriendo por detrás.",
+            "Ejemplo 2, esta frase tiene aproximadamente 400 caracteres. A medida que el texto crece, también lo hace la necesidad de gestionar mejor la sincronización para evitar que la impresión se sienta artificial. Una animación fluida puede mejorar la experiencia del usuario y dar la sensación de que algo más complejo está ocurriendo por detrás.",
             "Ejemplo 3, esta frase tiene aproximadamente 300 caracteres. Imprimir frases largas puede ser útil para simular una narración en consola. Cuanto más largo el texto, más evidente será el efecto visual de escritura progresiva, especialmente si el usuario presta atención a la velocidad.",
-            "Ejemplo 2, esta frase tiene aproximadamente 200 caracteres. A veces, una frase larga ayuda a ver cómo se comporta la impresión carácter por carácter cuando el texto empieza a ser un poco más largo.",
-            "No."};
-        for (String frase : frases) {
-            m.delayer(res, frase, System.out::print);
-            System.out.println("");
-            System.out.println("");
-        }
+            "Ejemplo 4, esta frase tiene aproximadamente 200 caracteres. A veces, una frase larga ayuda a ver cómo se comporta la impresión carácter por carácter cuando el texto empieza a ser un poco más largo.", //            "1.2.3."
+        };
+//        Si quisiese meter solo una frase, haría lo siguiente:
+//        m.delayer(new String[]{"Frase única a tratar"}, System.out::print);
+        m.delayer(sentences, System.out::print);
     }
 }
--- TextDelayerCons/metodo.java
+++ TextDelayerCons/metodo.java
@@ -4,6 +4,7 @@
  */
 package TextDelayerCons;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.logging.Level;
@@ -18,26 +19,44 @@ public class metodo {
     public metodo() {
     }
 
-    public void delayer(int num, String frase, Consumer<Character> cons) {
-        switch (num) {
+    public void delayer(String[] sentences, Consumer<Character> cons) {
+        //Comentar o eliminar y overridear con una selección de velocidad en los ajustes del programa
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Selecciona tu velocidad, por favor:");
+        System.out.println("1. Lento");
+        System.out.println("2. Normal");
+        System.out.println("3. Rapido");
+        System.out.println("4. Rapidísimo");
+        Integer res = Integer.valueOf(sc.nextLine());
+        sc.close();
+        switch (res) {
             case 1 ->
-                num = 4000;
+                res = 4000;
+            case 2 ->
+                res = 2000;
             case 3 ->
-                num = 1000;
+                res = 1000;
             case 4 ->
-                num = 500;
-            default ->
-                throw new IllegalArgumentException("Velocidad no válida");
-        }
-        int velocidad = (num * 1000) / frase.length();
-        for (char c : frase.toCharArray()) {
-            cons.accept(c);
-            try {
-                TimeUnit.MICROSECONDS.sleep(velocidad);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(metodo.class.getName()).log(Level.SEVERE, null, ex);
+                res = 500;
+            default -> {
+                sentences = new String[1];
+                sentences[0] = "El número que has introducido no es válido, por favor prueba de nuevo";
+                res = 2000;
             }
         }
-        System.out.println("");
+        for (String sentence : sentences) {
+            int velocidad = (res * 1000) / sentence.length();
+            for (char c : sentence.toCharArray()) {
+                cons.accept(c);
+                try {
+                    TimeUnit.MICROSECONDS.sleep(velocidad);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(metodo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+        }
     }
 }
